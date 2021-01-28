bf36e7d244f89e844b2b4cc5b8b5f6920cd74e03
Md shimul hossain <71894302+mdshimulhossain123738@users.noreply.github.com>
2021-01-28 22:39:03 +0600

    Update Log in

--- src/main/java/view/register/LogIn.java
+++ src/main/java/view/register/LogIn.java
@@ -4,11 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LogIn extends JFrame {
+    JLabel l1,l2;
+    JTextField t1;
+    JPasswordField t2;
+    JButton b1;
+
     public LogIn(String title) throws HeadlessException {
         super(title);
-        setSize(new Dimension(380,320));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        l1=new JLabel("Username");
+        l1.setBounds(100,55,60,30);
+        add(l1);
+        l2=new JLabel("Password");
+        l2.setBounds(100,100,60,30);
+        add(l2);
+        t1=new JTextField();
+        t1.setBounds(180,60,80,20);
+        add(t1);
+        t2=new JPasswordField();
+        t2.setBounds(180,105,80,20);
+        add(t2);
+        b1=new JButton("Login");
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+        b1.setBounds(150,150,70,20);
+        add(b1);
+        setLayout(null);
+        setBounds(400,200,400,300);
         setVisible(true);
-        setLocationRelativeTo(null);
     }
 }
